ed1404b47e0d228787fea257a3a8e5b9ca017548
JuliaFernanda146 <87870033+JuliaFernanda146@users.noreply.github.com>
2021-07-23 13:11:22 -0300

    Teste

---
+++ script.js
@@ -0,0 +1,150 @@
+"use strict";
+
+function Verifica()
+{
+
+    if(document.getElementById("1-Op-A").checked) {
+        document.querySelector("#Resposta1").innerHTML = "Resposta incorreta";
+        document.getElementById("Resposta1").style.backgroundColor = "red";
+    }
+    else 
+      if(document.getElementById('1-Op-B').checked) {
+        document.querySelector("#Resposta1").innerHTML = "Resposta incorreta";
+        document.getElementById("Resposta1").style.backgroundColor = "red";
+      }
+    else 
+      if(document.getElementById('1-Op-C').checked) {
+        document.querySelector("#Resposta1").innerHTML = "Resposta incorreta";
+        document.getElementById("Resposta1").style.backgroundColor = "red";
+      }
+    else 
+      if(document.getElementById('1-Op-D').checked) {
+        document.querySelector("#Resposta1").innerHTML = "Resposta correta";
+        document.getElementById("Resposta1").style.backgroundColor = "green";
+      }
+    else 
+      if(document.getElementById('1-Op-E').checked) {
+        document.querySelector("#Resposta1").innerHTML = "Resposta incorreta";
+        document.getElementById("Resposta1").style.backgroundColor = "red";
+      }
+
+
+
+
+      
+
+  if(document.getElementById("2-Op-A").checked) {
+      document.querySelector("#Resposta2").innerHTML = "Resposta incorreta";
+      document.getElementById("Resposta2").style.backgroundColor = "red";
+  }
+  else 
+    if(document.getElementById('2-Op-B').checked) {
+      document.querySelector("#Resposta2").innerHTML = "Resposta incorreta";
+      document.getElementById("Resposta2").style.backgroundColor = "red";
+    }
+  else 
+    if(document.getElementById('2-Op-C').checked) {
+      document.querySelector("#Resposta2").innerHTML = "Resposta incorreta";
+      document.getElementById("Resposta2").style.backgroundColor = "red";
+    }
+  else 
+    if(document.getElementById('2-Op-D').checked) {
+      document.querySelector("#Resposta2").innerHTML = "Resposta correta";
+      document.getElementById("Resposta2").style.backgroundColor = "green";
+    }
+  else 
+    if(document.getElementById('2-Op-E').checked) {
+      document.querySelector("#Resposta2").innerHTML = "Resposta incorreta";
+      document.getElementById("Resposta2").style.backgroundColor = "red";
+    }
+
+
+
+    
+      
+
+if(document.getElementById("3-Op-A").checked) {
+    document.querySelector("#Resposta3").innerHTML = "Resposta correta";
+    document.getElementById("Resposta3").style.backgroundColor = "green";
+}
+else 
+  if(document.getElementById('3-Op-B').checked) {
+    document.querySelector("#Resposta3").innerHTML = "Resposta incorreta";
+    document.getElementById("Resposta3").style.backgroundColor = "red";
+  }
+else 
+  if(document.getElementById('3-Op-C').checked) {
+    document.querySelector("#Resposta3").innerHTML = "Resposta incorreta";
+    document.getElementById("Resposta3").style.backgroundColor = "red";
+  }
+else 
+  if(document.getElementById('3-Op-D').checked) {
+    document.querySelector("#Resposta3").innerHTML = "Resposta incorreta";
+    document.getElementById("Resposta3").style.backgroundColor = "red";
+  }
+else 
+  if(document.getElementById('3-Op-E').checked) {
+    document.querySelector("#Resposta3").innerHTML = "Resposta incorreta";
+    document.getElementById("Resposta3").style.backgroundColor = "red";
+  }
+
+
+  
+    
+      
+
+if(document.getElementById("4-Op-A").checked) {
+  document.querySelector("#Resposta4").innerHTML = "Resposta incorreta";
+  document.getElementById("Resposta4").style.backgroundColor = "red";
+}
+else 
+if(document.getElementById('4-Op-B').checked) {
+  document.querySelector("#Resposta4").innerHTML = "Resposta incorreta";
+  document.getElementById("Resposta4").style.backgroundColor = "red";
+}
+else 
+if(document.getElementById('4-Op-C').checked) {
+  document.querySelector("#Resposta4").innerHTML = "Resposta incorreta";
+  document.getElementById("Resposta4").style.backgroundColor = "red";
+}
+else 
+if(document.getElementById('4-Op-D').checked) {
+  document.querySelector("#Resposta4").innerHTML = "Resposta incorreta";
+  document.getElementById("Resposta4").style.backgroundColor = "red";
+}
+else 
+if(document.getElementById('4-Op-E').checked) {
+  document.querySelector("#Resposta4").innerHTML = "Resposta correta";
+  document.getElementById("Resposta4").style.backgroundColor = "green";
+}
+
+
+    
+      
+
+if(document.getElementById("5-Op-A").checked) {
+  document.querySelector("#Resposta5").innerHTML = "Resposta incorreta";
+  document.getElementById("Resposta5").style.backgroundColor = "red";
+}
+else 
+if(document.getElementById('5-Op-B').checked) {
+  document.querySelector("#Resposta5").innerHTML = "Resposta incorreta";
+  document.getElementById("Resposta5").style.backgroundColor = "red";
+}
+else 
+if(document.getElementById('5-Op-C').checked) {
+  document.querySelector("#Resposta5").innerHTML = "Resposta incorreta";
+  document.getElementById("Resposta5").style.backgroundColor = "red";
+}
+else 
+if(document.getElementById('5-Op-D').checked) {
+  document.querySelector("#Resposta5").innerHTML = "Resposta incorreta";
+  document.getElementById("Resposta5").style.backgroundColor = "red";
+}
+else 
+if(document.getElementById('5-Op-E').checked) {
+  document.querySelector("#Resposta5").innerHTML = "Resposta correta";
+  document.getElementById("Resposta5").style.backgroundColor = "green";
+}
+
+}
